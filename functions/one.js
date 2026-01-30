b060bfce79aa0904f1ef249d2ed9b69ce08bd48b
sravanth pasupuleti <sravanthpasupuleti@gmail.com>
2026-01-30 12:01:23 +0530

    javascript functions

---
+++ functions/one.js
@@ -0,0 +1,14 @@
+// function add(a , b=2){
+// console.log(a + b)
+// }
+// add( 2, 4)
+
+// function dob(year){
+//     let current_year = new Date().getFullYear();
+//     return current_year-year
+// }
+// let age = dob(2003)s
+// console.log(age)
+
+let full_date = new Date().toLocaleTimeString();
+console.log(full_date)
