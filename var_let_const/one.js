f94a9a43d7b1990dde4f7cb73fdfeca045c444ef
sravanth pasupuleti <sravanthpasupuleti@gmail.com>
2026-02-04 20:58:58 +0530

    var let const in javascript

---
+++ var_let_const/one.js
@@ -0,0 +1,27 @@
+// var a = 10;
+// var a = 20;
+// console.log(a);  // 20
+
+// let b = 10;
+// b = 20;
+// console.log(b);  //in let re assign is possible 
+
+// const c = 10;
+// c = 20;
+// const c = 30;
+// console.log(c);
+
+var a = 50;
+let b = 20;
+const c = 30;
+b = 2000000;
+
+if(1){
+    var a = 500;
+    let b = 200;
+    const c = 300;
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ var_let_const/two.js
@@ -0,0 +1,16 @@
+var a = 10;
+var b = "skcns";
+
+if (a * b) {
+    return console.log("trueeee");
+}else{
+    console.log("falseee");
+}
+    
+console.log( 2 + undefined);
+
+//this values return true 
+//true , 1, " " , "233sdd"
+
+//this values return false
+//false , 0  , "" , undefined , NaN
